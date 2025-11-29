d7658ccd3e4ea844896a8f0ab8b9f21a7e8c3040
jjooxz <jpturra140900@gmail.com>
2025-11-28 21:40:08 -0300

    Warning if latency is 0 on pong command

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -4,6 +4,12 @@ use super::super::{Context, Error};
 #[poise::command(slash_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     let latency = ctx.ping().await.as_millis();
-    ctx.say(format!("🏓 Pong! Latência {:?}ms", latency)).await?;
+
+    if latency == 0 {
+        ctx.say("🏓 Pong! O bot acabou de ligar, então não foi possivel calcular a latência ainda, espere um pouco e rode esse comando mais tarde ;3").await?;
+        return Ok(())
+    }
+
+    ctx.say(format!("🏓 Pong! Latência {}ms ", latency)).await?;
     Ok(())
 }
