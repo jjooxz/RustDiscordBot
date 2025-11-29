8d50b8e93bd425c0ae636d0832871f3452cf0c3a
jjooxz <jpturra140900@gmail.com>
2025-11-28 21:32:20 -0300

    Now check for latency on ping command

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -3,6 +3,7 @@ use super::super::{Context, Error};
 // Comando que responde com "Pong!"
 #[poise::command(slash_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say("🏓 Pong!").await?;
+    let latency = ctx.ping().await.as_millis();
+    ctx.say(format!("🏓 Pong! Latência {:?}ms", latency)).await?;
     Ok(())
 }
